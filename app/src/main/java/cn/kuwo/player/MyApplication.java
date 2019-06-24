4fe30ffcca623de17f86b7218c709a769469c1b9
zhouwangzhen <601637652@qq.com>
2019-06-24 10:56:11 +0800

    modify leancloud host

--- app/src/main/java/cn/kuwo/player/MyApplication.java
+++ app/src/main/java/cn/kuwo/player/MyApplication.java
@@ -11,8 +11,6 @@ import com.tencent.bugly.crashreport.CrashReport;
 import cn.kuwo.player.util.RealmHelper;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
-import io.sentry.Sentry;
-import io.sentry.android.AndroidSentryClientFactory;
 
 public class MyApplication extends Application {
     private  static Context context;
@@ -21,6 +19,14 @@ public class MyApplication extends Application {
         super.onCreate();
         context=getApplicationContext();
         Logger.addLogAdapter(new AndroidLogAdapter());
+        // 配置 SDK 储存
+        AVOSCloud.setServer(AVOSCloud.SERVER_TYPE.API, "https://lean.aobeef.cn");
+        // 配置 SDK 云引擎
+        AVOSCloud.setServer(AVOSCloud.SERVER_TYPE.ENGINE, "https://lean.aobeef.cn");
+        // 配置 SDK 推送
+        AVOSCloud.setServer(AVOSCloud.SERVER_TYPE.PUSH, "https://lean.aobeef.cn");
+        // 配置 SDK 即时通讯
+        AVOSCloud.setServer(AVOSCloud.SERVER_TYPE.RTM, "https://lean.aobeef.cn");
         AVOSCloud.initialize(this,"s7XAC1LB5TyCB1o2fObRxnXh","dy57fdLtOpnHBpskQF6qAi7U");
         Realm.init(this);
         RealmConfiguration configuration=new RealmConfiguration.Builder()
